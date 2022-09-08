d9fe29b8962d31de8102bb3e7c20f0fb09b9b456
Luis Cuende <me@luisivan.net>
2022-09-08 11:29:50 +0100

    Add website redirect

--- next.config.js
+++ next.config.js
@@ -3,6 +3,12 @@ module.exports = {
   async redirects() {
     return [
       {
+        source: '/',
+        destination: 'https://nation3.org',
+        permanent: false,
+        basePath: false,
+      },
+      {
         source: '/discord',
         destination: 'https://discord.gg/a4jsHKwrAa',
         permanent: false,
